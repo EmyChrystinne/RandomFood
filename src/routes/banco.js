97f716bc3e597de81926762a35ee805eab35a18c
Jardel Cleyson <jardelcleyson@gmail.com>
2024-05-27 21:52:12 -0300

    banco fix updates

--- src/routes/banco.js
+++ src/routes/banco.js
@@ -1,55 +1,80 @@
-// Importar a biblioteca de cliente Oracle
+// const firebase = require("firebase/app");
+// const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+// const { getFirestore } = require('firebase-admin/firestore');
+const firebase = require('firebase/app');
+const { initializeApp } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
+const admin = require('firebase-admin');
+
 const express = require('express');
 const router = express.Router();
-const oracledb = require('oracledb');
+
 require('dotenv').config();
 
-// Configurações de conexão com o banco de dados Oracle Autonomous Database
-const dbConfig = {
-  user: process.env.USERDB,
-  password: process.env.PASSWORDDB,
-  connectString: process.env.CONNECTSTRINGDB
+const serviceAccount = require("../data/serviceAccountKey");
+
+const adminConfig = {
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://randomfoodapp-c286c-default-rtdb.firebaseio.com/'
 };
-let consulta=[];
-async function closeOpenConnections() {
- // Conectar ao banco de dados
- let connection = await oracledb.getConnection(dbConfig);
- openConnections.push(connection);
- if (connection != null)
- console.log('Conexão ao banco de dados estabelecida');
- else
- console.log('Conexão ao banco de dados falhou');
 
- // Consultas SQL para obter todas as categorias, preços, refeições e localizações
- let queryCategorias = 'SELECT * FROM categorias';
- //let queryPreco = 'SELECT * FROM preco';
- //let queryRefeicao = 'SELECT * FROM refeicao';
- //let queryLocalizacao = 'SELECT * FROM localizacao';
+// Inicialize o Firebase Admin SDK apenas uma vez
+const firebaseApp = admin.initializeApp(adminConfig);
 
- // Executar as consultas
- const resultCategorias = await connection.execute(queryCategorias);
- //const resultPreco = await connection.execute(queryPreco);
- //const resultRefeicao = await connection.execute(queryRefeicao);
- //const resultLocalizacao = await connection.execute(queryLocalizacao);
+module.exports = firebaseApp;
 
- // Fechar a conexão com o banco de dados
- await connection.close();
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
 
- // Extrair os dados dos resultados das consultas
- const categorias = resultCategorias.rows;
-//  const preco = resultPreco.rows;
-//  const refeicao = resultRefeicao.rows;
-//  const localizacao = resultLocalizacao.rows;
- console.log('Categorias:', categorias);
- 
- 
-}
-// Rota para obter todas as categorias
-router.get('/categorias', async (req, res) => {
+// Inicialize o Firebase
+firebase.initializeApp(firebaseConfig);
+console.log('Firebase inicializado');
+
+// Referência para o Firestore
+
+
+const db = getFirestore();
+console.log('db:', db);
+
+
+const collectionRef = db.collection('default');
+collectionRef.get().then((querySnapshot) => {
+  querySnapshot.forEach((doc) => {
+    console.log("dado",doc.id, '=>', doc.data());
+  });
+}).catch((error) => {
+  console.log('Erro ao recuperar documentos:', error);
+});
+
+// Obter dados das categorias, preços, localizações e nomes de restaurantes
+router.get('/botoes', async (req, res) => {
   try {
-    console.log('==',consulta)
-    console.log(closeOpenConnections)
-    // Enviar os dados como respostares.json({ categorias, preco, refeicao, localizacao });
+    const categoriasSnapshot = await db.collection('Categoria').get(); 
+    const categorias = categoriasSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    console.log('categorias:', categorias);
+
+    const precosSnapshot = await db.collection('Preço').get();
+    const precos = precosSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+
+    const localizacoesSnapshot = await db.collection('Localização').get();
+    const localizacoes = localizacoesSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+
+    const restaurantesSnapshot = await db.collection('NOME').get();
+    const nome = restaurantesSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+
+    const refeicao = await db.collection('Refeição').get();
+    const refeicoes = refeicao.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    // console.log('refeicoes:', refeicoes);
+
+    // res.json({ categoria, preço, localizacão, nome, refeição });
+    res.json({ 'Nome': nome, 'Categoria': categorias, 'Preço': precos, 'Localização': localizacoes, 'Refeição': refeicoes});
   } catch (error) {
     console.error('Erro ao obter dados:', error);
     res.status(500).json({ error: 'Erro ao obter dados' });
@@ -57,3 +82,7 @@ router.get('/categorias', async (req, res) => {
 });
 
 module.exports = router;
+
+
+
+
